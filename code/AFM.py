c3f03c999a2e86a0d507293b06bcf6efad28b04f
sssjjjaaa <37531907+SeongJinAhn@users.noreply.github.com>
2019-08-14 23:01:19 +0900

    Update AFM.py

--- code/AFM.py
+++ code/AFM.py
@@ -10,12 +10,25 @@ class afm(nn.Module):
         self.GPU = config.GPU_available
         if self.GPU:
             self.ToTensor = torch.cuda.LongTensor
+            self.device = torch.device('cuda')
     
-        self.person_embed = nn.Embedding(num_user,config.embedding_dim)
-        self.prediction = nn.Linear(3,1)
+        self.person_embed = nn.Embedding(num_user*3,config.embedding_dim)
+        self.prediction = nn.Linear(config.embedding_dim,1)
         self.attention= Attention(config.embedding_dim)
 
+        self.p = nn.Parameter(torch.rand(1,config.embedding_dim))
+        self.gen_bias = nn.Parameter(torch.rand(1))
+
     def forward(self,group,config):
+        first = self.FirstOrder(group,config)
+        second = self.AttentiveSecond(group,config)
+        return self.gen_bias + first + second
+
+    def FirstOrder(self,group,config):
+        
+        return 0
+
+    def AttentiveSecond(self,group,config):
         gr_len = len(group)
         gr_embed = []
         a = torch.FloatTensor()
@@ -30,15 +43,24 @@ class afm(nn.Module):
             if config.GPU_available:
                 b = torch.cuda.FloatTensor()
     
+            v_ij=torch.Tensor()
+            a_ij=torch.Tensor()
+
+            if config.GPU_available :
+                v_ij=v_ij.to(self.device)
+                a_ij=a_ij.to(self.device)   
+
             for i, v_i in enumerate(gr):
                 for v_j in gr[i+1:]:
-                    v_ij = torch.mul(v_i,v_j)
-                    a_ij = self.attention(v_ij)
+                    element_wise = torch.mul(v_i,v_j)
+                    v_ij = torch.cat((v_ij,element_wise.reshape(-1,1)),dim=-1)
+                    a_ij = torch.cat((a_ij,self.attention(element_wise)))
 
-                    b = torch.cat((b,a_ij))
-            a = torch.cat((a,b.reshape(1,3)),dim=0)
-        a = self.prediction(a)
-        a = nn.Sigmoid()(a) * 2 -1
+            v_ij = v_ij.reshape(config.embedding_dim,-1)
+            a_hat_ij = nn.Softmax(dim=0)(a_ij) # relative impact
+
+            product = torch.mm(self.p,torch.mul(v_ij,a_hat_ij)).sum()
+            a = torch.cat((a,product.reshape(1)))
 #        a = nn.Softmax(dim=0)(a.flatten())
         return a
         
@@ -47,12 +69,15 @@ class afm(nn.Module):
 class Attention(nn.Module):
     def __init__(self, embedding_dim, drop_ratio=0):
         super(Attention, self).__init__()
-        self.L1 = nn.Linear(embedding_dim,4)
-        self.L2 = nn.Linear(4,1)
+        self.L1 = nn.Linear(embedding_dim,200)
+        self.L2 = nn.Linear(200,30)
+        self.L3 = nn.Linear(30,1)
 
 
     def forward(self, x):
         out = self.L1(x)
         out = nn.Sigmoid()(out)
         out = self.L2(out)
+        out = nn.Sigmoid()(out)
+        out = self.L3(out)
         return out
